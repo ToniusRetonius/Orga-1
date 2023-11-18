29b8ae6e40a0b3e095878c5633c693671b85ef2e
Tomas Lopez <toto.melli074@gmail.com>
2023-11-18 09:53:59 -0300

    Parcial

---
+++ Parcial/ejercicios_1_riscv.asm
@@ -0,0 +1,22 @@
+.text:
+
+# inicializar programa
+
+# recorrer arreglo y acumular
+
+exit:
+    # Imprime la suma
+    mv a0, t4
+    li a7, 34
+    ecall
+
+    # Termina el programa.
+    li a0, 0
+    li a7, 93
+    ecall
+
+.data:
+arr:
+.word	0xffffffff, 0x95555555, 0xf4444444, 0xf1111111
+.word	0xffffff00, 0xf5005555, 0x95444444, 0xf1113311
+.word	0xff00ffff, 0xf5550055, 0xa4444433, 0xa1551111  
